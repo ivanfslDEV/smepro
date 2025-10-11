6242ff556c38a970a5c91cc84f940657265356d9
Ivan <ivanfslima@gmail.com>
2025-10-11 15:32:21 +0100

    feat: start structuring sidebar component

--- src/app/(panel)/dashboard/layout.tsx
+++ src/app/(panel)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { SidebarDashboard } from "./_components/sidebar"
+
 export default function DashboardLayout({
     children,
 }: {
@@ -5,7 +7,9 @@ export default function DashboardLayout({
 }) {
     return (
         <>
-            {children}
+            <SidebarDashboard>
+                {children}
+            </SidebarDashboard>
         </>
     )
 }
