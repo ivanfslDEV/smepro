136a1d0a85f65a4816202edaff6eeac93ca619d0
Ivan <ivanfslima@gmail.com>
2025-10-16 21:32:12 +0100

    feat(services): create price mask

---
+++ src/app/(panel)/dashboard/services/_components/dialog-service-form.tsx
@@ -0,0 +1,33 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Name is Mandatory"}),
+    price: z.string().min(1, { message: "Price is Mandatory"}),
+    hours: z.string(),
+    minutes: z.string(),
+});
+
+export interface UserDialogServiceFormProps {
+    initialValues?: {
+        name: string;
+        price: string;
+        hours: string;
+        minutes: string;
+    };
+}
+
+export type DialogServiceFormData = z.infer<typeof formSchema>;
+
+export function useDialogServiceForm() {
+    return useForm<DialogServiceFormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            price: "",
+            hours: "0",
+            minutes: "0",
+        }
+    });
+}
--- src/app/(panel)/dashboard/services/_components/dialog-service.tsx
+++ src/app/(panel)/dashboard/services/_components/dialog-service.tsx
@@ -1,8 +1,41 @@
 "use client"
 
 import { DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { DialogServiceFormData, useDialogServiceForm } from "./dialog-service-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function DialogService(){
+
+    const form = useDialogServiceForm();
+
+    async function onSubmit(values:DialogServiceFormData) {
+        
+    }
+
+    function changeCurrency(event: React.ChangeEvent<HTMLInputElement>){
+        let {value} = event.target;
+        value = value.replace(/\D/g, '');
+
+        if(value){
+            value = (parseInt(value, 10) / 100).toFixed(2);
+            value = value.replace('.', ',');
+            value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+        }
+
+        event.target.value = value;
+        form.setValue("price", value);
+    }
+
     return(
         <>
             <DialogHeader>
@@ -12,9 +45,76 @@ export function DialogService(){
                 </DialogDescription>
             </DialogHeader>
 
-            <div>
-                12342
-            </div>
+            <Form {...form}>
+                <form 
+                    className="space-y-2"
+                    onSubmit={form.handleSubmit(onSubmit)}
+                >
+                    <div className="flex flex-col">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">Service Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Service Name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="price"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">Service price</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} placeholder="120,00" onChange={changeCurrency}  />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    
+                    <p className="font-semibold">Service Duration:</p>
+                    <div className="grid grid-cols-2 gap-3">
+                        <FormField
+                            control={form.control}
+                            name="hours"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">Hours:</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="1" min="0" type="number" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="minutes"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">Minutes:</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="1" min="0" type="number" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <Button type="submit" className="w-full font-semibold text-white">
+                        Create Service
+                    </Button>
+                </form>
+            </Form>
         </>
     )
 }
