ae40052596b422825866152d13dd2658f07ea305
Ivan <ivanfslima@gmail.com>
2025-10-19 21:34:37 +0100

    feat(dashboard): create appointment buttons

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
 import getSession from "@/lib/getSession";
+import { Calendar } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { ButtonCopyLink } from "./_components/button-copy-link";
 
 export default async function Dashboard() {
   const session = await getSession();
@@ -8,8 +12,25 @@ export default async function Dashboard() {
   }
   
   return (
-    <div className="Dashboard">
-      Pag Dashboard
-    </div>
+    <main>
+      <div className="space-x-2 flex items-center justify-end">
+        <Link
+          href={`/business/${session.user?.id}`}
+          target="_blank"
+        >
+          <Button
+            className="bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-[0]"
+          >
+            <Calendar className="w-5 h-5" />
+            <span>New Appointment</span>
+          </Button>
+        </Link>
+
+        <ButtonCopyLink userId={session.user?.id}/>
+      </div>
+
+      <section className="grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4"></section>
+
+    </main>
   );
 }
