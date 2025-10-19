ab4616432093e40df464ee5909fa1dee541e9d91
Ivan <ivanfslima@gmail.com>
2025-10-19 22:37:32 +0100

    feat(reminder): delete

--- src/app/(panel)/dashboard/_components/reminder-list.tsx
+++ src/app/(panel)/dashboard/_components/reminder-list.tsx
@@ -2,14 +2,32 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Reminder } from "@/generated/prisma";
 import { Plus, Trash } from "lucide-react";
+import { deleteReminder } from "../_actions/delete-reminder";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface ReminderListProps {
     reminder: Reminder[]
 }
 
 export function ReminderList({ reminder }: ReminderListProps){
+    const router = useRouter();
+
+    async function handleDeleteReminder(id: string) {
+        const response = await deleteReminder({reminderId: id});
+
+        if(response.error){
+            toast.error(response.error);
+            return;
+        }
+
+        toast.success(response.data);
+        router.refresh();
+    }
+
     return (
         <div className="flex flex-col gap-3">
             <Card>
@@ -31,20 +49,25 @@ export function ReminderList({ reminder }: ReminderListProps){
                         </p>
                     )}
 
-                    {reminder.map((item) => (
-                        <article 
-                            key={item.id}
-                            className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
-                        >
-                            <p className="text-sm lg:text-base">{item.description}</p>
-                            <Button
-                                className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
-                                size="sm"
+                    <ScrollArea
+                        className="h-[340px] lg:max-h-[calc(100vh-15rem)] pr-0 w-full flex-1"
+                    >
+                        {reminder.map((item) => (
+                            <article 
+                                key={item.id}
+                                className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
                             >
-                                <Trash className="w-4 h-4 text-white"/>
-                            </Button>
-                        </article>
-                    ))}
+                                <p className="text-sm lg:text-base">{item.description}</p>
+                                <Button
+                                    className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
+                                    size="sm"
+                                    onClick={ () => handleDeleteReminder(item.id) }
+                                >
+                                    <Trash className="w-4 h-4 text-white"/>
+                                </Button>
+                            </article>
+                        ))}
+                    </ScrollArea>
                 </CardContent>
             </Card>
         </div>
