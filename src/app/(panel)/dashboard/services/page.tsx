e1ffd9e0bb1f3a2a8d99700003ab6cf426c9033d
Ivan <ivanfslima@gmail.com>
2025-10-21 23:13:22 +0100

    feat: apply cache and streaming UI

--- src/app/(panel)/dashboard/services/page.tsx
+++ src/app/(panel)/dashboard/services/page.tsx
@@ -1,6 +1,7 @@
 import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
 import { ServicesContent } from "./_components/service-content";
+import { Suspense } from "react";
 
 export default async function Services() {
     const session = await getSession();
@@ -9,6 +10,9 @@ export default async function Services() {
     }
 
     return (
-       <ServicesContent userId={session.user?.id!} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ServicesContent userId={session.user?.id!} />
+      </Suspense>
+       
     )
 }
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,6 +4,8 @@ import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
 import { getProfessionals } from "./_data-access/get-professionals";
 
+export const revalidate = 120;
+
 export default async function Home() {
 
   const professionals = await getProfessionals();
