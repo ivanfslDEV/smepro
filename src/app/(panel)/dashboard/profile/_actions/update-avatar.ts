b1a33a967cba1c8e4ab1759270e135d229e4d4fa
Ivan <ivanfslima@gmail.com>
2025-10-25 00:11:04 +0100

    feat(profile): save new image url on DB

---
+++ src/app/(panel)/dashboard/profile/_actions/update-avatar.ts
@@ -0,0 +1,42 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import { revalidatePath } from "next/cache";
+
+export async function updateProfileAvatar({avatarUrl}:{avatarUrl: string}) {
+    const session = await auth();
+
+    if(!session?.user?.id){
+        return{
+            error: "User not found"
+        }
+    }
+
+    if(!avatarUrl){
+        return{
+            error: "Erro updating image"
+        }
+    }
+
+    try{
+        await prisma.user.update({
+            where:{
+                id: session?.user?.id
+            },
+            data:{
+                image: avatarUrl
+            }
+        });
+
+        revalidatePath("/dashboard/profile");
+
+        return{
+            data: "Image Updated"
+        }
+    }catch(err){
+        return{
+            error: "Erro updating image"
+        }
+    }
+}
