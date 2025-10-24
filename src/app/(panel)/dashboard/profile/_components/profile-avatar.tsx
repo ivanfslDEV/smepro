e2aa4494e3ab80071a35ea0e8bbb9d92d07750f4
Ivan <ivanfslima@gmail.com>
2025-10-24 23:07:53 +0100

    feat(profile): create upload image method

--- src/app/(panel)/dashboard/profile/_components/profile-avatar.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile-avatar.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Image from "next/image";
 import imgTest from "../../../../../../public/foto1.png"
-import { Upload } from "lucide-react";
+import { Loader, Upload } from "lucide-react";
+import { toast } from "sonner";
 
 interface AvatarProfileProps {
     avatarUrl: string | null;
@@ -12,17 +13,65 @@ interface AvatarProfileProps {
 
 export function AvatarProfile({avatarUrl, userId}: AvatarProfileProps){
     const [previewImage, setPreviewImage] = useState(avatarUrl);
+    const [loading, setLoading] = useState(false);
+
+    async function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        if(e.target.files && e.target.files[0]){
+            setLoading(true);
+            const image = e.target.files[0];
+
+            if(image.type != "image/jpeg" && image.type !== "image/png"){
+                toast.error("Invalid Format");
+                return;
+            }
+
+            const newFileName = `${userId}`;
+            const newFile = new File([image], newFileName, {type: image.type});
+
+            const urlImage = await uploadImage(newFile);
+        }
+    }
+
+    async function uploadImage(image:File): Promise<string | null> {
+        try{
+            toast("Uploading Image");
+
+            const formData = new FormData();
+
+            formData.append("file", image);
+            formData.append("userId", userId);
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/image/upload`, {
+                method: "POST",
+                body: formData
+            });
+
+            const data = await response.json();
+
+            if(!response.ok){
+                return null;
+            }
+
+            toast("Image uploaded!");
+
+            return data as string;
+
+        }catch(err){
+            return null;
+        }
+    }
 
     return(
         <div className="relative w-40 h-40 md:w-48 md:h-48">
 
             <div className="relative flex items-center justify-center w-full h-full">
                 <span className="absolute cursor-pointer z-[2] bg-slate-50/80 p-2 rounded-full shadow-xl">
-                    <Upload size={16} color="#131313"/>
+                    {loading ? <Loader size={16} color="#131313" className="animate-spin"/> : <Upload size={16} color="#131313"/>}
                 </span>
                 <input 
                     type="file"
                     className="opacity-0 cursor-pointer relative z-50 w-48 h-48"
+                    onChange={handleChange}
                 />
             </div>
 
---
+++ src/app/api/image/upload/routes.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+    const formData = await request.formData();
+
+    const file = formData.get("file") as File;
+    const userId = formData.get("userId") as string;
+
+    return NextResponse.json({ ok: true})
+}
