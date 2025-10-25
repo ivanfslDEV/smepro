c90e507c5eca3a00ac70d34ad8d82efbe0211157
Ivan <ivanfslima@gmail.com>
2025-10-25 08:35:46 +0100

    feat(auth): allow login with google

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -24,7 +24,7 @@ export function Header() {
     ]
 
     async function handleLogin() {
-        await handleRegister("github")
+        await handleRegister("google")
     }
 
     const NavLinks = () => (
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,11 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma  from "./prisma";
 import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  providers: [GitHub],
+  providers: [GitHub, Google],
 })
