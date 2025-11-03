7a5846b5fa6556584098b8d6cb7eda6ad6d03151
ivanfslDEV <ivanfslima@gmail.com>
2025-11-03 22:45:14 +0000

    feat(auth): remove github as a provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./prisma";
@@ -9,7 +8,6 @@ import bcrypt from "bcryptjs";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub,
     Google,
     Credentials({
       name: "Credentials",
