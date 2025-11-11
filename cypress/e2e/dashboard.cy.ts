7199eef02261761e14fd755cc585d6feb8ca2f90
ivanfslDEV <ivanfslima@gmail.com>
2025-11-11 22:34:37 +0000

    test(dashboard): Improve e2e test

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -4,36 +4,38 @@ describe("Dashboard", () => {
     cy.visit("/dashboard");
   });
 
-  it("Open dashboard", () => {
+  it("should display main sections", () => {
     cy.contains("Schedule").should("be.visible");
     cy.contains("Reminders").should("be.visible");
   });
-});
 
-describe("Notes", () => {
-  beforeEach(() => {
-    cy.login();
-    cy.visit("/dashboard");
-  });
+  context("Reminders", () => {
+    const reminderText = "Buy milk";
 
-  it("Create Reminder", () => {
-    cy.get('[role="dialog"]').should("not.exist");
-    cy.get('[data-cy="open-reminder-form"]').click();
-    cy.get('[data-cy="reminder-dialog-form"]').should("be.visible");
-    cy.contains("New Reminder").should("be.visible");
-    cy.contains("Create a new reminder").should("be.visible");
-    cy.get('button[data-cy="create-reminder-button"]').should("be.disabled");
-    cy.get('textarea[data-cy="reminder-description-field"]').type("Buy milk");
-    cy.get('button[data-cy="create-reminder-button"]').should(
-      "not.be.disabled"
-    );
-    cy.get('button[data-cy="create-reminder-button"]').click();
-    cy.get('[data-cy="reminder-list-item"]').contains("Buy milk");
-  });
+    it("should create a new reminder", () => {
+      cy.get('[data-cy="open-reminder-form"]').click();
+      cy.get('[data-cy="reminder-dialog-form"]').should("be.visible");
 
-  it("Remove Reminder", () => {
-    cy.get('[data-cy="reminder-list-item"]').contains("Buy milk");
-    cy.get('button[data-cy="remove-button-reminder-item"]').click();
-    cy.contains("You have 0 reminder(s)").should("be.visible");
+      cy.contains("New Reminder").should("be.visible");
+      cy.contains("Create a new reminder").should("be.visible");
+
+      cy.get('button[data-cy="create-reminder-button"]').should("be.disabled");
+
+      cy.get('textarea[data-cy="reminder-description-field"]').type(
+        reminderText
+      );
+
+      cy.get('button[data-cy="create-reminder-button"]')
+        .should("not.be.disabled")
+        .click();
+
+      cy.get('[data-cy="reminder-list-item"]').contains(reminderText);
+    });
+
+    it("should remove an existing reminder", () => {
+      cy.get('[data-cy="reminder-list-item"]').contains(reminderText);
+      cy.get('button[data-cy="remove-button-reminder-item"]').click();
+      cy.contains("You have 0 reminder(s)").should("be.visible");
+    });
   });
 });
