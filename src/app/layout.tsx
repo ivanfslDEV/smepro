372cadb7ec83531d93f8960c35c7de32acfa6ce2
ivanfslDEV <ivanfslima@gmail.com>
2026-01-06 23:33:06 +0000

    chore: add SCSS support and configure basic setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionAuthProvider } from "@/components/session-auth";
 import { QueryClientContext } from "@/providers/queryClient";
+import "@/styles/main.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionAuthProvider>
-          <QueryClientContext>
-            {children}
-          </QueryClientContext>
+          <QueryClientContext>{children}</QueryClientContext>
         </SessionAuthProvider>
       </body>
     </html>
