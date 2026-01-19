b955f3b082e4c631a1d9e4d9b330cb31ba910495
ivanfslDEV <ivanfslima@gmail.com>
2026-01-19 21:35:50 +0000

    fix: correct formatting and improve readability in ButtonCopyLink component

--- src/app/(panel)/dashboard/_components/button-copy-link.tsx
+++ src/app/(panel)/dashboard/_components/button-copy-link.tsx
@@ -1,19 +1,21 @@
-"use client"
+"use client";
 
 import { IconButton } from "@/components/ui/icon-button";
 import { LinkIcon } from "lucide-react";
 import { toast } from "sonner";
 
-export function ButtonCopyLink({ userId }: {userId: string}){
-    async function handleCopyLink() {
-        await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_URL}/business/${userId}`);
+export function ButtonCopyLink({ userId }: { userId: string }) {
+  async function handleCopyLink() {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/business/${userId}`
+    );
 
-        toast("Link copied!");
-    }
+    toast.success("Link copied!");
+  }
 
-    return(
-        <IconButton label="Copy scheduling link" onClick={handleCopyLink}>
-            <LinkIcon className="w-5 h-5"/>
-        </IconButton>
-    )
+  return (
+    <IconButton label="Copy scheduling link" onClick={handleCopyLink}>
+      <LinkIcon className="w-5 h-5" />
+    </IconButton>
+  );
 }
